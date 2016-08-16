3061e573b62c6b7a067dfb91be8535e1eb67b8c3
DanTheMan0000 <dittdanie@gmail.com>
2016-08-15 22:23:11 -0500

    broken state, need help

--- index.js
+++ index.js
@@ -7,4 +7,26 @@ require('babel-core/register')({
     'babel-plugin-transform-async-to-generator'
   ]
 });
-console.log("This is my first")
+
+
+
+
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = Router();
+
+
+router.get('/:8000', (ctx, next) => {
+  ctx.body = ('Hello World');
+  ctx.type = 'json';
+});
+
+
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+app.listen(8000, () => {console.log('Server listening on http://127.0.0.1:8000' );});
